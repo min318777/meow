a54a3d3676b0f5acf08e67415be2f81a80f81a05
DESKTOP-U6CF5AQ\user <min318777@naver.com>
2025-05-31 16:24:49 +0900

    refactor:consumer모듈의 yml에 api의 yml을 include하도록 설정

--- api/src/main/java/com/min/meow/MeowApplication.java
+++ api/src/main/java/com/min/meow/MeowApplication.java
@@ -3,7 +3,7 @@ package com.min.meow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.min.meow", "com.min.kafka"})
 public class MeowApplication {
 
 	public static void main(String[] args) {
--- consumer/src/main/java/com/min/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/min/consumer/ConsumerApplication.java
@@ -4,7 +4,7 @@ package com.min.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.min.consumer", "com.min.meow"})
 public class ConsumerApplication {
 
     public static void main(String[] args) {
