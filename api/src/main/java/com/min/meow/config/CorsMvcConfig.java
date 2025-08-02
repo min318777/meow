7ff1ce07c082987751ff7bfed75bacf12ab77862
DESKTOP-U6CF5AQ\user <min318777@naver.com>
2025-08-02 13:45:53 +0900

    빌드전 커밋

--- api/src/main/java/com/min/meow/config/CorsMvcConfig.java
+++ api/src/main/java/com/min/meow/config/CorsMvcConfig.java
@@ -4,17 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-// 리액트와 연동시 cors에러 방지 클래스
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .exposedHeaders("Set-Cookie")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://meow-front.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
-
     }
 }
